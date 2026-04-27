7d4411d481e464e44db2422efa6ee86e2b88a914
Syshacker20 <mixakrasav95@gmail.com>
2026-04-27 13:06:17 +0300

    S7 - Group Service - Столяров А.С.

--- S7/models.py
+++ S7/models.py
@@ -1,41 +1,34 @@
 from peewee import *
 
-# Подключение к локальной базе данных SQLite
+# Подключение к БД
 db = SqliteDatabase('group_service_s7.db')
 
 class BaseModel(Model):
     class Meta:
         database = db
 
-class Curator(BaseModel):
-    """Справочник кураторов (3НФ)"""
-    full_name = CharField(unique=True, null=False)
-
 class GroupStatus(BaseModel):
-    """Справочник статусов (3НФ)"""
+    """Справочник: Активна / Выпустилась"""
     title = CharField(unique=True, null=False)
 
 class Group(BaseModel):
-    """Таблица учебных групп"""
-    name = CharField(unique=True, null=False)
-    formation_year = IntegerField(null=False)
-    # Внешние ключи для связи таблиц
+    """Сущность Учебная группа"""
+    name = CharField(unique=True, null=False)           # Номер группы
+    formation_year = IntegerField(null=False)          # Год поступления
+    education_base = IntegerField(null=False)          # 9 или 11 класс
     status = ForeignKeyField(GroupStatus, backref='groups', null=False)
-    curator = ForeignKeyField(Curator, backref='groups', null=False)
+    curator_id = IntegerField(null=False)              # ID из сервиса профилей
 
 def init_db():
-    """Создание таблиц и заполнение начальными данными"""
+    """Инициализация таблиц"""
     db.connect()
-    db.create_tables([Curator, GroupStatus, Group], safe=True)
+    db.create_tables([GroupStatus, Group], safe=True)
     
-    # Наполнение справочников, если они пусты
+    # Заполнение справочника статусов
     if GroupStatus.select().count() == 0:
         GroupStatus.create(title="Активна")
         GroupStatus.create(title="Выпустилась")
-    
-    if Curator.select().count() == 0:
-        Curator.create(full_name="Столяров А.С.")
 
 if __name__ == "__main__":
     init_db()
-    print("Группа №7: База данных успешно инициализирована.")
+    print("S7 Group Service: БД успешно инициализирована.")
