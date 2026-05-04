d41f4343c8c404f59b715dfc6d1430c92e4a4c0b
OrlovMatvey <deceptor27@mail.ru>
2026-05-04 17:02:28 +0300

    .

--- S13/models.py
+++ S13/models.py
@@ -1,37 +1,55 @@
-from peewee import *
+"""Модуль моделей базы данных."""
 import datetime
+from peewee import SqliteDatabase, Model, CharField, DateTimeField, \
+    ForeignKeyField, IntegerField
 
 db = SqliteDatabase('work_programs.db')
 
+
 class BaseModel(Model):
+    """Базовая модель."""
+
     class Meta:
         database = db
 
+
 class WorkProgram(BaseModel):
+    """Модель рабочих программ."""
+
     title = CharField(null=False)
     file_path = CharField(null=False)
     version = CharField(null=False, default="1.0")
     created_at = DateTimeField(default=datetime.datetime.now, null=False)
 
-class Specialty(BaseModel):
-    code = CharField(unique=True, null=False)
-    name = CharField(null=False)
+    class Meta:
+        indexes = (
+            (('title', 'version'), True),
+        )
+
 
 class ProgramAssignment(BaseModel):
-    program = ForeignKeyField(WorkProgram, backref='assignments', null=False)
-    specialty = ForeignKeyField(Specialty, backref='programs', null=False)
-    discipline_id = IntegerField(null=False) # То самое поле
+    """Модель назначений."""
+
+    program = ForeignKeyField(
+        WorkProgram,
+        backref='assignments',
+        on_delete='CASCADE'
+    )
+    specialty_id = IntegerField(null=False)
+    discipline_id = IntegerField(null=False)
 
     class Meta:
         indexes = (
-            (('program', 'specialty', 'discipline_id'), True),
+            (('program', 'specialty_id', 'discipline_id'), True),
         )
 
+
 def init_db():
+    """Инициализация БД."""
     db.connect()
-    db.create_tables([WorkProgram, Specialty, ProgramAssignment])
-    print("БД инициализирована.")
+    db.create_tables([WorkProgram, ProgramAssignment])
+    print("БД Work Program Service инициализирована.")
+
 
 if __name__ == "__main__":
     init_db()
-
