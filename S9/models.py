f197accf0bb58b9a797988ca5c0a8dda80d4fa2b
wkizzz <pokap0795@gmail.com>
2026-06-01 15:28:01 +0300

    .

--- S9/models.py
+++ S9/models.py
@@ -11,7 +11,7 @@ class BaseModel(Model):
 class Group(BaseModel):
     id = PrimaryKeyField()
     speciality = CharField(max_length=100)
-    course_year = IntegerField()
+    course_year = IntegerField(constraints=[Check('course_year BETWEEN 1 AND 4')])
 
     class Meta:
         table_name = 'groups'
@@ -41,8 +41,8 @@ class StudentMovement(BaseModel):
     movement_type_id = ForeignKeyField(MovementType, backref='movements')
     from_group_id = ForeignKeyField(Group, null=True, backref='from_movements')
     to_group_id = ForeignKeyField(Group, null=True, backref='to_movements')
-    movement_date = DateField()
-    reason = TextField()
+    movement_date = DateField(constraints=[Check("movement_date <= date('now')")])
+    reason = CharField(max_length=500)
     order_number = CharField(max_length=50, unique=True)
     created_by = CharField(max_length=100)
     is_active = BooleanField(default=True)
