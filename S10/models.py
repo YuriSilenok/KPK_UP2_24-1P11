88418763de01efa8275d8c1efb867619f1d31d42
Maxwin512 <hoptiti@mail.ru>
2026-06-03 14:29:54 +0300

    Update models.py

--- S10/models.py
+++ S10/models.py
@@ -7,9 +7,12 @@ class BaseModel(Model):
         database = db
 
 class Position(BaseModel):
+    id = AutoField()
     name = CharField(unique=True)
+    is_active = BooleanField(default=True)
 
 class EmployeeStatus(BaseModel):
+    id = AutoField()
     employee_id = IntegerField()
     position = ForeignKeyField(Position, backref="employee_statuses")
     rate = FloatField()
@@ -19,16 +22,20 @@ class EmployeeStatus(BaseModel):
     is_active = BooleanField(default=True)
 
 class Vacation(BaseModel):
+    id = AutoField()
     employee_id = IntegerField()
     start_date = DateField()
     end_date = DateField()
     vacation_type = CharField()
+    is_active = BooleanField(default=True)
 
 class SickLeave(BaseModel):
+    id = AutoField()
     employee_id = IntegerField()
     start_date = DateField()
     end_date = DateField()
     document_number = CharField()
+    is_active = BooleanField(default=True)
 
 def init_db():
     db.connect(reuse_if_open=True)
@@ -39,5 +46,5 @@ def init_db():
         SickLeave
     ])
 
-
-
+if __name__ == "__main__":
+    init_db()
