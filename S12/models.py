3ed5e81972bd270f31d57a6e21a19c733efc999a
olesya <ol.pavlova555@gmail.com>
2026-06-03 12:12:24 +0300

    .

--- S12/models.py
+++ S12/models.py
@@ -21,7 +21,7 @@ class CurriculumPlan(BaseModel):
 
 class Subject(BaseModel):
     id = AutoField()
-    curriculum_plan = ForeignKeyField(CurriculumPlan, backref='subjects', on_delete='CASCADE', null=False)
+    curriculum_plan_id = ForeignKeyField(CurriculumPlan, backref='subjects', on_delete='CASCADE', null=False)
     name = CharField(null=False, constraints=[Check("name != ''")])
     semester = IntegerField(null=False, constraints=[Check("semester BETWEEN 1 AND 12")])
     is_active = BooleanField(default=True)
@@ -29,7 +29,7 @@ class Subject(BaseModel):
 
 class Hours(BaseModel):
     id = AutoField()
-    subject = ForeignKeyField(Subject, backref='hours', on_delete='CASCADE', null=False)
+    subject_id = ForeignKeyField(Subject, backref='hours', on_delete='CASCADE', null=False, unique=True)
     lecture_hours = IntegerField(default=0, null=False, constraints=[Check("lecture_hours >= 0")])
     practice_hours = IntegerField(default=0, null=False, constraints=[Check("practice_hours >= 0")])
     is_active = BooleanField(default=True)
@@ -37,9 +37,14 @@ class Hours(BaseModel):
 
 class Assessment(BaseModel):
     id = AutoField()
-    subject = ForeignKeyField(Subject, backref='assessments', on_delete='CASCADE', null=False)
+    subject_id = ForeignKeyField(Subject, backref='assessments', on_delete='CASCADE', null=False)
     type = CharField(null=False, constraints=[Check("type IN ('exam', 'zachet')")])
     is_active = BooleanField(default=True)
+
+    class Meta:
+        indexes = (
+            (('subject_id', 'type'), True),
+        )
 
 
 def init_db():
