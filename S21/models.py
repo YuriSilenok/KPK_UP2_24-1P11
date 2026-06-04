a5e84196024af5b656fa6f19aef985162ff1af85
Nikita-Kabanov <kabanovn988@gmail.com>
2026-06-04 23:42:54 +1400

    Update models.py

--- S21/models.py
+++ S21/models.py
@@ -7,7 +7,6 @@ class BaseModel(Model):
         database = db
 
 class HolidayType(BaseModel):
-    """Справочник типов: holiday (праздник) или vacation (каникулы)"""
     id = AutoField()
     name = CharField(max_length=50, null=False)
     code = CharField(max_length=8, unique=True, null=False)
@@ -17,7 +16,6 @@ class HolidayType(BaseModel):
         table_name = 'holiday_type'
 
 class Holiday(BaseModel):
-    """Праздники"""
     id = AutoField()
     name = CharField(max_length=100, null=False)
     date = DateField(null=False)
@@ -32,7 +30,6 @@ class Holiday(BaseModel):
         )
 
 class VacationPeriod(BaseModel):
-    """Каникулы (период)"""
     id = AutoField()
     name = CharField(max_length=100, null=False)
     start_date = DateField(null=False)
@@ -49,20 +46,22 @@ class VacationPeriod(BaseModel):
         )
 
     def save(self, *args, **kwargs):
+        # Data integrity constraint: end_date must be >= start_date
         if self.end_date < self.start_date:
-            raise ValueError("end_date должен быть >= start_date")
+            raise ValueError("end_date must be >= start_date")
         return super().save(*args, **kwargs)
 
 def init_db():
     db.connect()
     db.create_tables([HolidayType, Holiday, VacationPeriod], safe=True)
     
+    # Add default types
     if HolidayType.select().count() == 0:
-        HolidayType.create(name='Праздник', code='holiday')
-        HolidayType.create(name='Каникулы', code='vacation')
+        HolidayType.create(name='Holiday', code='holiday')
+        HolidayType.create(name='Vacation', code='vacation')
     
     db.close()
 
 if __name__ == '__main__':
     init_db()
-    print("База данных создана.")
+    print("Database created.")
